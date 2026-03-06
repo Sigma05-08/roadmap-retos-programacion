6284661272cae63be781284858c358881d7a5589
Sigma05-08 <scarlett08085@gmail.com>
2026-03-05 19:27:59 -0500

    Create Sigma05-08.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/Sigma05-08.py
@@ -0,0 +1,22 @@
+#https://www.python.org/downloads/
+
+#comentario en un alinea
+
+"""
+comentario en
+varias
+lineas
+"""
+'''
+comentario 
+en
+varias lineas
+'''
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+
+cadena_de_texto = "cadena de texto"
+otra_cadena_de_texto = 'otra cadena de texto'
+print("¡Hola, python!")
